9ec0a42c88ce3c2be3f6a4a7ac60e5a6eddb68b7
matekuna92 <kunamate66@gmail.com>
2019-08-13 21:43:47 +0200

    class variables, immutabilibity, array methods

--- firstAng/src/app/app.component.ts
+++ firstAng/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstAng';
+  korte = 'korte';
+
+  arr = [1,2,3,4,5,6,7];  // osztályváltozók
+
+  constructor()
+  {
+      console.log('Hey');
+      // console.log(arr); osztályváltozó nem használható a függvényben -> this.arr -ral kell hivatkozni rá, jelezni kell, hogy 
+      // osztályváltozót akarok használni:   
+      console.log(this.arr);
+
+      /*
+      console.log(this.arr.filter(function(item)
+      {
+        return item > 4;
+      }));
+      */
+
+      // ES6+ verzió: 
+      console.log( this.arr.filter((item) => item > 4) );
+      console.log( this.arr.map((item) => item*2) );
+
+      // első futásnál veszi a 0. és 1. elemet, ezekre hajtja végre a műveletet
+      console.log( this.arr.reduce((x,y) => x + y) );
+
+      // immutabilibity, változtathatatlanság: nem változtatható az eredeti tömb/object
+
+      // új tömböt állítok elő, az eredeti tömb nem változik !
+      const tomb = this.arr.filter((item) => item > 4);
+      console.log('Tomb:', tomb);
+      console.log('Original array:', this.arr);
+    }
 }
